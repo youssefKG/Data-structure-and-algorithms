cf766735e274aa7082b74d8ef2da4ff975f9f9e0
youssef <yousseftaoussi894@gmail.com>
2024-02-25 00:47:50 +0100

    add bublesort algo

---
+++ bubleSort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+void insertArray(int *arr, int length) {
+  for (int i = 0; i < length; i++) {
+    printf("T[%d] : ", i);
+    scanf("%d", &arr[i]);
+  }
+}
+void displayArr(int *arr, int length) {
+  for (int i = 0; i < length; i++)
+    printf("| %d ", arr[i]);
+  printf("|\n");
+}
+void swap(int *num1, int *num2) {
+  int temp = *num1;
+  *num1 = *num2;
+  *num2 = temp;
+}
+int main() {
+  int *arr;
+  int length;
+  printf("insert the size of array : ");
+  scanf("%d", &length);
+  arr = (int *)malloc(length * sizeof(int));
+  insertArray(arr, length);
+  printf("before : \n");
+  displayArr(arr, length);
+  // buble sort
+  for (int i = 0; i < length - 1; i++) {
+    for (int j = 0; j < length - i - 1; j++) {
+      if (arr[j] > arr[j + 1])
+        swap(&arr[j], &arr[j + 1]);
+    }
+  }
+  printf("after : \n");
+  displayArr(arr, length);
+  return 0;
+}
